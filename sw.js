852c3862a20dc6a07e43d96c75737bcf98c79b44
linjiefeng37-maker <linjiefeng37@gmail.com>
2026-09-11 03:13:08 +0800

    刷新第三页手机缓存

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = "english365-pwa-v9";
+const CACHE_NAME = "english365-pwa-v10";
 const APP_SHELL = [
   "./",
   "./index.html",
   "./styles.css?v=shadowing-20",
   "./data.js",
   "./vocabulary-days-4-50.js?v=desktop-day8-50-11",
-  "./app.js?v=daily-variety-22",
+  "./app.js?v=daily-play-once-23",
   "./manifest.webmanifest",
   "./icon-180.png",
   "./icon-192.png",
